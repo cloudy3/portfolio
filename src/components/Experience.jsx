1b3ccfa1e35c9b412fab265a182e7a34dd2922a8
cloudy3 <cjingfeng98@gmail.com>
2024-07-21 20:16:41 +0800

    Initial commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,10 @@
+const Experience = () => {
+  return (
+    <div id="experience" className="section">
+      <h1>Experience</h1>
+      <p>An overview of my work experience.</p>
+    </div>
+  );
+};
+
+export default Experience;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,10 @@
+const Portfolio = () => {
+  return (
+    <div id="projects" className="section">
+      <h1>Projects</h1>
+      <p>Here are some of the projects I have worked on.</p>
+    </div>
+  );
+};
+
+export default Portfolio;
