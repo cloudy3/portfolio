4a9467af9bc2da6428eb1c71de9561c47a38ae53
cloudy3 <cjingfeng98@gmail.com>
2024-07-28 22:52:01 +0800

    feat: add navbar and  portfolio data

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,19 @@
 import Contact from "./Contact";
 import Experience from "./Experience";
 import Hero from "./Hero";
+import CanvasLoader from "./Loader";
+import Navbar from "./Navbar";
 import Portfolio from "./Portfolio";
+import Position from "./Position";
 import TokyoCanvas from "./Tokyo";
 
-export { Contact, Experience, Hero, Portfolio, TokyoCanvas };
+export {
+  CanvasLoader,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Portfolio,
+  Position,
+  TokyoCanvas,
+};
---
+++ src/data/index.js
@@ -0,0 +1,64 @@
+export const navLinks = [
+  {
+    id: "hero",
+    title: "Hero",
+  },
+  {
+    id: "portfolio",
+    title: "Portfolio",
+  },
+  {
+    id: "experience",
+    title: "Experience",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const experiences = [
+  {
+    title: "YouTube Content Creator",
+    company_name: "Self Employed",
+    date: "2016 - Present",
+    details: [
+      "Built a subscriber base of over <span style='color: white;'>500,000 subscribers</span> by creating video content to help programmers.",
+      "Crafted visually appealling programming videos that have garnered over <span style='color: white;'>30,000,000 views</span>.",
+      "Produced high-quality educational and entertaining videos for clients including <span style='color: white;'>Intel, JetBrains, and MicroCenter</span>.",
+    ],
+  },
+  {
+    title: "Software Engineer",
+    company_name: "Bernhard Schulte",
+    date: "Jul 2023 - Present",
+    details: [
+      "Developed asdfghj including <span style='color: white;'>yes</span>.",
+      "<span style='color: white;'>Designed and developed</span> some website and mobile app.",
+      "<span style='color: white;'>Managed um</span> yes.",
+    ],
+  },
+  {
+    title: "Software Engineer Intern",
+    company_name: "Resync",
+    date: "Jul 2021 - Dec 2021",
+    details: [
+      "End-to-end development for a <span style='color: white;'>start up</span> as a full-stack software engineer.",
+      "Developed and maintained <span style='color: white;'>yes</span>, ensuring wow.",
+      "<span style='color: white;'>Collaborated with a team</span> to something",
+    ],
+  },
+  {
+    title: "Software Engineer Intern",
+    company_name: "wecome",
+    date: "Nov 2020 - Dec 2020",
+    details: [
+      "Worked on a delivery app <span style='color: white;'>(Cates)</span> for delivery riders to deliver food from campus canteens to NTU Halls, widely adopted by students during the COVID-19 pandemic.",
+      "Developed the hybrid app using Flutter, integrating a web app into a native app for the start-up company.",
+    ],
+  },
+];
+
+const portfolio = [];
+
+export { experiences, portfolio };
