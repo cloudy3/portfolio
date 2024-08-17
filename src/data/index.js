fd01218abf0cbf55465fcba81bda27bb51cc7ff4
cloudy3 <cjingfeng98@gmail.com>
2024-08-17 20:21:10 +0800

    feat: update experiences

--- src/data/index.js
+++ src/data/index.js
@@ -19,9 +19,15 @@ const experiences = [
     company_name: "Bernhard Schulte",
     date: "Jul 2023 - Present",
     details: [
-      "Developed asdfghj including <span style='color: white;'>yes</span>.",
-      "<span style='color: white;'>Designed and developed</span> some website and mobile app.",
-      "<span style='color: white;'>Managed um</span> yes.",
+      "Contributed to the development of <span style='color: white;'>two websites and two mobile apps</span> using <span style='color: white;'>Flutter</span>",
+      "Implemented <span style='color: white;'>Getx State Management</span> to <span style='color: white;'>optimize app performance</span> and enhance efficiency.",
+      "Deployed <span style='color: white;'>Google Cloud Functions</span> to support various backend processes.",
+      "<span style='color: white;'>Automated image compression</span> with Firebase Extensions, <span style='color: white;'>saving 96% storage space</span> and improving user experience.",
+      "Integrated <span style='color: white;'>Microsoft APIs to enable seamless OneDrive file picker functionality</span> on web platforms, simplifying the process of uploading large quantities of images for clients.",
+      "Revamped Firestore database architecture, <span style='color: white;'>optimised data structure and updated over 3000 existing documents</span> to enhance scalability and performance.",
+      "Led the <span style='color: white;'>end-to-end process</span> of launching Android apps on <span style='color: white;'>Google Play Store</span>.",
+      "Implemented <span style='color: white;'>Firebase buckets</span> in <span style='color: white;'>multiple regions</span>",
+      "Established <span style='color: white;'>local database</span> using <span style='color: white;'>Hive</span> for offline app usage",
     ],
   },
   {
@@ -29,9 +35,14 @@ const experiences = [
     company_name: "Resync",
     date: "Jul 2021 - Dec 2021",
     details: [
-      "End-to-end development for a <span style='color: white;'>start up</span> as a full-stack software engineer.",
-      "Developed and maintained <span style='color: white;'>yes</span>, ensuring wow.",
-      "<span style='color: white;'>Collaborated with a team</span> to something",
+      "<span style='color: white;'>End-to-end development</span> for a start up as a <span style='color: white;'>full-stack software engineer</span>.",
+      "Implemented mobile notification features using <span style='color: white;'>Firebase, Firestore and Firebase Cloud Messaging</span>",
+      "<span style='color: white;'>Deployed</span> Flutter app to the <span style='color: white;'>Google Play Store</span> and the <span style='color: white;'>Apple App Store</span>",
+      "Designed and implemented components and services using <span style='color: white;'>Angular</span>",
+      "Created <span style='color: white;'>React</span> components and used <span style='color: white;'>Redux</span> for State Management",
+      "Learnt how to set up a backend server using <span style='color: white;'>Postgres</span> and <span style='color: white;'>Docker</span>",
+      "Implemented <span style='color: white;'>Unit Tests</span> using<span style='color: white;'> Mocha, Chai and Sinon</span>",
+      "Created UI/UX designs on <span style='color: white;'>Figma</span> for a web portal",
     ],
   },
   {
@@ -39,8 +50,8 @@ const experiences = [
     company_name: "wecome",
     date: "Nov 2020 - Dec 2020",
     details: [
-      "Worked on a delivery app <span style='color: white;'>(Cates)</span> for delivery riders to deliver food from campus canteens to NTU Halls, widely adopted by students during the COVID-19 pandemic.",
-      "Developed the hybrid app using Flutter, integrating a web app into a native app for the start-up company.",
+      "Worked on a delivery app <span style='color: white;'>(Cates)</span> for delivery riders to deliver food from campus canteens to <span style='color: white;'>NTU Halls</span>, <span style='color: white;'>widely adopted by students</span> during the COVID-19 pandemic.",
+      "Developed the hybrid app using Flutter, <span style='color: white;'>integrating a web app into a native app</span> for the start-up company.",
     ],
   },
 ];
